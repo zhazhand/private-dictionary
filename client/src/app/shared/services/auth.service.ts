d40fd3b3baa26f539fef4333d6ca5a3fe122f400
Andrii Zhazhko <kfkrgf@gmail.com>
2025-08-21 23:54:36 +0300

    reworked auth service

--- client/src/app/shared/services/auth.service.ts
+++ client/src/app/shared/services/auth.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { CookieService } from "ngx-cookie-service";
 import { Observable } from "rxjs";
 import { tap } from "rxjs/operators";
 import { User } from "@interfaces/user";
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
-  private token: string | null = null;
+  token: string | null = null;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private cookieService: CookieService,
+  ) {}
   register(user: User): Observable<User> {
     return this.http.post<User>("/api/auth/register", user);
   }
@@ -16,27 +20,25 @@ export class AuthService {
   login(user: User): Observable<{ token: string }> {
     return this.http.post<{ token: string }>("/api/auth/login", user).pipe(
       tap(({ token }) => {
-        localStorage.setItem("auth-token", token);
-        this.setToken(token);
+        this.cookieService.set("auth-token", token);
+        this.token = token;
       }),
     );
   }
-  setToken(token: string | null) {
-    this.token = token;
-  }
 
   getToken(): string | null {
+    if (!this.token) {
+      this.token = this.cookieService.get("auth-token");
+    }
     return this.token;
   }
 
   isAuthenticated(): boolean {
-    const token = localStorage.getItem("auth-token");
-    this.setToken(token);
-    return !!token;
+    return !!this.getToken();
   }
 
   logOut() {
-    this.setToken(null);
-    localStorage.clear();
+    this.token = null;
+    this.cookieService.delete("auth-token");
   }
 }
