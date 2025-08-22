2cf509d33f8a403c15096268b980a6ba62cfd732
Andrii Zhazhko <kfkrgf@gmail.com>
2025-08-22 22:35:42 +0300

    reworked auth-guard, added token.interceptor

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -6,15 +6,16 @@ import {
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
-import { provideHttpClient } from "@angular/common/http";
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { tokenInterceptor } from "./core/middleware/token.interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([tokenInterceptor])),
     provideAnimationsAsync(),
   ],
 };
