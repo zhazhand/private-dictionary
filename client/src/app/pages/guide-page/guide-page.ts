682deb1d6ea49434f3c33d580f4c0d79606a5ee8
zhazhand <kfkrgf@gmail.com>
2025-09-13 23:29:46 +0300

    reworked guide

--- client/src/app/pages/guide-page/guide-page.ts
+++ client/src/app/pages/guide-page/guide-page.ts
@@ -1,14 +1,19 @@
 import { Component } from "@angular/core";
+import { CommonModule } from '@angular/common';
 import { ToggleSwitcher } from "@reusable/toggle-switcher/toggle-switcher";
 
 @Component({
   selector: "app-guide-page",
-  imports: [ToggleSwitcher],
+  imports: [ToggleSwitcher,CommonModule],
   templateUrl: "./guide-page.html",
   styleUrl: "./guide-page.less",
 })
 export class GuidePage {
+  englishVersion: string = 'eng';
+  ukrainianVersion: string= 'ua';
+  selectedLanguage: string = this.englishVersion;
+
   setLanguage(lang: string): void {
-    console.log(lang);
+    this.selectedLanguage = lang;
   }
 }
