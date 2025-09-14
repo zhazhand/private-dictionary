73a2ee0245a33095dcfca024350726afb56635f7
zhazhand <kfkrgf@gmail.com>
2025-09-15 01:00:51 +0300

    fixed sorting

--- client/src/app/shared/pipes/order-by/order-by-pipe.ts
+++ client/src/app/shared/pipes/order-by/order-by-pipe.ts
@@ -1,23 +1,27 @@
-import { Pipe, PipeTransform } from "@angular/core";
-import { ListItem } from "@interfaces/list-item.interface";
-import { getClearWord } from "app/shared/utils";
+import { Pipe, PipeTransform } from '@angular/core';
+import { ListItem } from '@interfaces/list-item.interface';
+import { getClearWord } from 'app/shared/utils';
 
 @Pipe({
-  name: "OrderBy",
+  name: 'OrderBy',
 })
 export class OrderByPipe implements PipeTransform {
   transform(array: ListItem[], args: string): ListItem[] {
     let flag = true,
       arg: string = args;
 
-    if (arg === "date") {
+    if (arg === 'date') {
       flag = false;
     }
     array.sort((a: ListItem, b: ListItem) => {
-      if (getClearWord(a[arg] as string) < getClearWord(b[arg] as string)) {
+      if (
+        getClearWord(a[arg] as string).toLowerCase() <
+        getClearWord(b[arg] as string).toLowerCase()
+      ) {
         return -1;
       } else if (
-        getClearWord(a[arg] as string) > getClearWord(b[arg] as string)
+        getClearWord(a[arg] as string).toLowerCase() >
+        getClearWord(b[arg] as string).toLowerCase()
       ) {
         return 1;
       } else {
