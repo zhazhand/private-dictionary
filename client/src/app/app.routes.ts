4f6bf7b9dd3b8f5e7755c366a12f428716551ffc
zhazhand <kfkrgf@gmail.com>
2025-09-18 22:59:16 +0300

    added lazy loading

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,12 +1,12 @@
-import { Routes } from "@angular/router";
-import { authGuard } from "app/core/middleware/auth-guard.guard";
-import { routePath } from "@constants/constants";
-import { GuidePage } from "@pages/guide-page/guide-page";
-import { NotFoundPage } from "@pages/not-found-page/not-found-page";
-import { LoginPage } from "@pages/login-page/login-page";
-import { RegistrationPage } from "@pages/registration-page/registration-page";
-import { ItemForm } from "@reusable/item-form/item-form";
-import { BasePage } from "@pages/base-page/base-page";
+import { Routes } from '@angular/router';
+import { authGuard } from 'app/core/middleware/auth-guard.guard';
+import { routePath } from '@constants/constants';
+import { GuidePage } from '@pages/guide-page/guide-page';
+import { NotFoundPage } from '@pages/not-found-page/not-found-page';
+import { LoginPage } from '@pages/login-page/login-page';
+import { RegistrationPage } from '@pages/registration-page/registration-page';
+import { ItemForm } from '@reusable/item-form/item-form';
+import { BasePage } from '@pages/base-page/base-page';
 
 const {
   guide,
@@ -23,7 +23,7 @@ const {
 } = routePath;
 
 export const routes: Routes = [
-  { path: "", redirectTo: guide, pathMatch: "full" },
+  { path: '', redirectTo: guide, pathMatch: 'full' },
   { path: guide, component: GuidePage },
   { path: irregular, component: BasePage, canActivate: [authGuard] },
   {
@@ -43,21 +43,42 @@ export const routes: Routes = [
     component: ItemForm,
     canActivate: [authGuard],
   },
-  { path: separable, component: BasePage, canActivate: [authGuard] },
   {
-    path: `${separable}/${routePath.new}`,
-    component: ItemForm,
+    path: separable,
+    loadComponent: () =>
+      import('@pages/base-page/base-page').then((m) => m.BasePage),
     canActivate: [authGuard],
   },
-  { path: `${separable}/${id}`, component: ItemForm, canActivate: [authGuard] },
-  { path: gerund, component: BasePage, canActivate: [authGuard] },
+  {
+    path: `${separable}/${routePath.new}`,
+    loadComponent: () =>
+      import('@reusable/item-form/item-form').then((m) => m.ItemForm),
+    canActivate: [authGuard],
+  },
+  {
+    path: `${separable}/${id}`,
+    loadComponent: () =>
+      import('@reusable/item-form/item-form').then((m) => m.ItemForm),
+    canActivate: [authGuard],
+  },
+  {
+    path: gerund,
+    loadComponent: () =>
+      import('@pages/base-page/base-page').then((m) => m.BasePage),
+    canActivate: [authGuard],
+  },
   {
     path: `${gerund}/${routePath.new}`,
     component: ItemForm,
     canActivate: [authGuard],
   },
   { path: `${gerund}/${id}`, component: ItemForm, canActivate: [authGuard] },
-  { path: infinitive, component: BasePage, canActivate: [authGuard] },
+  {
+    path: infinitive,
+    loadComponent: () =>
+      import('@pages/base-page/base-page').then((m) => m.BasePage),
+    canActivate: [authGuard],
+  },
   {
     path: `${infinitive}/${routePath.new}`,
     component: ItemForm,
@@ -68,14 +89,30 @@ export const routes: Routes = [
     component: ItemForm,
     canActivate: [authGuard],
   },
-  { path: phrases, component: BasePage, canActivate: [authGuard] },
   {
-    path: `${phrases}/${routePath.new}`,
-    component: ItemForm,
+    path: phrases,
+    loadComponent: () =>
+      import('@pages/base-page/base-page').then((m) => m.BasePage),
     canActivate: [authGuard],
   },
-  { path: `${phrases}/${id}`, component: ItemForm, canActivate: [authGuard] },
-  { path: stative, component: BasePage, canActivate: [authGuard] },
+  {
+    path: `${phrases}/${routePath.new}`,
+    loadComponent: () =>
+      import('@reusable/item-form/item-form').then((m) => m.ItemForm),
+    canActivate: [authGuard],
+  },
+  {
+    path: `${phrases}/${id}`,
+    loadComponent: () =>
+      import('@reusable/item-form/item-form').then((m) => m.ItemForm),
+    canActivate: [authGuard],
+  },
+  {
+    path: stative,
+    loadComponent: () =>
+      import('@pages/base-page/base-page').then((m) => m.BasePage),
+    canActivate: [authGuard],
+  },
   {
     path: `${stative}/${routePath.new}`,
     component: ItemForm,
@@ -84,5 +121,5 @@ export const routes: Routes = [
   { path: `${stative}/${id}`, component: ItemForm, canActivate: [authGuard] },
   { path: login, component: LoginPage },
   { path: registration, component: RegistrationPage },
-  { path: "**", component: NotFoundPage },
+  { path: '**', component: NotFoundPage },
 ];
