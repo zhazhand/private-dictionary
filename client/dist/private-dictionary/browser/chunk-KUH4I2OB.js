35c800ec4a9cfd3c5c11099950dfdbf4e911b717
zhazhand <kfkrgf@gmail.com>
2025-10-03 22:48:21 +0300

    reverted updating structure

--- client/dist/private-dictionary/browser/chunk-KUH4I2OB.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-Z3A5FS5P.js";import"./chunk-KFAJNYHZ.js";import"./chunk-WRWXI3SN.js";import"./chunk-6IJFH3QL.js";export{a as ItemForm};
--- client/dist/private-dictionary/browser/chunk-NN3IPU25.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-A2SBM4YW.js";import"./chunk-KFAJNYHZ.js";import"./chunk-6IJFH3QL.js";export{a as BasePage};
